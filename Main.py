d2539d98eef2227243b9fb726827493b0b13a46b
Gopikaak3 <gopikaak3@gmail.com>
2023-06-11 22:32:45 +0530

    Camera Changed to Intel Realsense Camera.

--- Main.py
+++ Main.py
@@ -1,26 +1,31 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import pyrealsense2 as rs
 
 #initiation
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-cap = cv2.VideoCapture(0)
-cap.set(3, 1920)
-cap.set(4, 1080)
-cap.set(6, cv2.VideoWriter.fourcc('M', 'J', 'P', 'G'))
+# cap = cv2.VideoCapture(0)
+# cap.set(3, 1920)
+# cap.set(4, 1080)
+# cap.set(6, cv2.VideoWriter.fourcc('M', 'J', 'P', 'G'))
 
-# Curl counter variables
+
+# variables
 whileLoopCounter = 0 
 stage = None
 tilt = 0.0
+tiltDirection = ""
+rightShoulder = [0,0,0]
+leftShoulder = [0,0,0]
 
 
 #Calculate angle function
 def calculate_angle(a,b,c):
     a = np.array(a) # First
     b = np.array(b) # Mid
-    c = np.array(c) # End
+    c = np.array(c) # last
     
     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
     angle = np.abs(radians*180.0/np.pi)
@@ -30,15 +35,23 @@ def calculate_angle(a,b,c):
         
     return angle 
 
+# Configure realsense
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+pipeline.start(config)
+
 # Setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
+    while True:
+        frames = pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+        color_image = np.asanyarray(color_frame.get_data())
         
         # Recolor image to RGB
-        image = cv2.flip(frame, 1)
+        image = cv2.flip(color_image, 1)
         image.flags.writeable = False
-      
+
         # Make detection
         results = pose.process(image)
         
@@ -51,8 +64,8 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             landmarks = results.pose_landmarks.landmark
             
             #taking Right and Left Shoulder coordinate points
-            rightShoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-            leftShoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            rightShoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].visibility]
+            leftShoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].visibility]
 
             refferenceShoulder = [0,0]
             # taking reference point on shoulder
@@ -67,9 +80,9 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             
             # Calculate angle
             whileLoopCounter +=1
-            if(whileLoopCounter ==10):
-                tilt = calculate_angle(First, Mid, last)
-                whileLoopCounter=0           
+            if(whileLoopCounter == 10):
+                tilt = calculate_angle(Firstq, Mid, last)
+                whileLoopCounter = 0
         except:
             pass
 
@@ -83,6 +96,8 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         cv2.putText(image, ": " + format(tilt, ".2f"), (150,50), 
                     cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
 
+#        cv2.putText(image, "Visibility : " + format(rightShoulder[2], ".2f") + format(leftShoulder[2], ".2f"), (150,50), 
+#                   cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
         # Render detections
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
