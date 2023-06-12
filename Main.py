b5bcd5839539ae8a0cb9fc5224195a8123b2e7ac
Gopikaak3 <gopikaak3@gmail.com>
2023-06-12 22:52:53 +0530

    Code optimization

--- Main.py
+++ Main.py
@@ -3,23 +3,26 @@ import mediapipe as mp
 import numpy as np
 import pyrealsense2 as rs
 
-#Calculate angle function
-def calculate_angle(a,b,c):
-    a = np.array(a) # First
-    b = np.array(b) # Mid
-    c = np.array(c) # last
-    
-    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-    angle = np.abs(radians*180.0/np.pi)
-    
-    if angle >180.0:
-        angle = 360-angle
-        
-    return angle 
+ 
 
 
 #Calculate angle using intel realsense camera
 def findTilt():
+    #Calculate angle function
+    def calculate_angle(a,b,c):
+        a = np.array(a) # First
+        b = np.array(b) # Mid
+        c = np.array(c) # last
+        
+        radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+        angle = np.abs(radians*180.0/np.pi)
+        
+        if angle >180.0:
+            angle = 360-angle
+            
+        return angle
+
+    
     #initiation
     mp_drawing = mp.solutions.drawing_utils
     mp_pose = mp.solutions.pose
@@ -141,7 +144,8 @@ def deviceNotFound():
     cap.release()
     cv2.destroyAllWindows()
 
-
+#Check device conneted or not
+#Run the tilt detection, if device conneted
 pipeline = rs.pipeline()
 ctx = rs.context()
 
